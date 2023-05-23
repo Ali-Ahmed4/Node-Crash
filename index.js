05ba8df7ddf02bd99282edea2c246f5570269fba
Ali-Ahmed1 <ali.ahmed@jogo.ai>
2023-05-23 21:24:03 +0500

    revert back to redis

--- index.js
+++ index.js
@@ -8,60 +8,47 @@ const app = express()
 app.use(express.json())
 
 
-/* const client = redis.createClient({host,port}); */
+const client = redis.createClient({host,port});
 
 
 
-/* const getRedis =  async (key) => {
+const getRedis =  async (key) => {
 
   const response = await client.get(key)
-
   if(response == null)
   {
     return null
   }
-
   return response
-
-} */
+}
 
 const setRedis =  async (req,res) => {
-  const { content } = req.body
-
-  /* const get =  await getRedis(content) */
-  
-  const response = await axios.get(`http://jsonplaceholder.typicode.com/${content}`)
-    
-    /* const cachedContent = await client.set(content, JSON.stringify(response.data), "EX","40") */
-
-  res.status(200).json({content: response.data})
-
-  /* if(get?.length > 0)
+  try 
   {
-    res.status(200).json(JSON.parse(get))
+    const { content } = req.body
+
+    const get =  await getRedis(content)
+  
+    if(get?.length > 0)
+    {
+      res.status(200).json(JSON.parse(get))
+    }
+    else 
+    {
+      const response = await axios.get(`http://jsonplaceholder.typicode.com/${content}`)
+      const cachedContent = await client.set(content, JSON.stringify(response.data), "EX","40")
+      res.status(200).json({content: response.data})
+    }
   }
-  else 
+  catch(error) 
   {
-    
-
-    fetch(`http://jsonplaceholder.typicode.com/${content}`)
-    .then((response) => response.json())
-    .then( async (jsonData) => {
-      
-     const cachedContent = await client.set(content, JSON.stringify(jsonData), "EX","40")
-
-     res.status(200).json({content: jsonData})
-
-    })
-  } */
-  
-  
+    res.send(error)
+  }
 }
 
 
 
-/* app.get("/",getRedis) */
-app.get("/", (req, res) => { res.send("its working!!!")})
+app.get("/",getRedis)
 app.post("/",setRedis)
 
 
